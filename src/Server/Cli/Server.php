f3e40e1ba72687f3e296b6ebbeae8603c4ab7899
Yurun <admin@yurunsoft.com>
2021-06-09 10:33:17 +0800

    重构 server/start 改为 swoole/start

---
+++ src/Server/Cli/Server.php
@@ -0,0 +1,179 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Swoole\Server\Cli;
+
+use Imi\App;
+use Imi\Cache\CacheManager;
+use Imi\Cli\Annotation\Command;
+use Imi\Cli\Annotation\CommandAction;
+use Imi\Cli\Annotation\Option;
+use Imi\Cli\ArgType;
+use Imi\Cli\Contract\BaseCommand;
+use Imi\Cli\Tools\Imi\Imi as ToolImi;
+use Imi\Config;
+use Imi\Event\Event;
+use Imi\Pool\PoolManager;
+use Imi\Server\ServerManager;
+use Imi\Swoole\Server\Contract\ISwooleServer;
+use Imi\Swoole\Util\Imi as SwooleImiUtil;
+use Imi\Util\Imi;
+
+/**
+ * @Command("swoole")
+ */
+class Server extends BaseCommand
+{
+    /**
+     * 开启服务
+     *
+     * @CommandAction(name="start")
+     * @Option(name="workerNum", type=ArgType::INT, required=false, comments="工作进程数量")
+     * @Option(name="daemon", shortcut="d", type=ArgType::STRING, required=false, comments="是否启用守护进程模式。加 -d 参数则使用守护进程模式。如果后面再跟上文件名，则会把标准输入和输出重定向到该文件")
+     *
+     * @param string|bool $d
+     */
+    public function start(?int $workerNum, $d): void
+    {
+        Event::one('IMI.SWOOLE.MAIN_COROUTINE.AFTER', function () use ($d) {
+            $this->outImi();
+            $this->outStartupInfo();
+            PoolManager::clearPools();
+            CacheManager::clearPools();
+            Event::trigger('IMI.SWOOLE.SERVER.BEFORE_START');
+            // 创建服务器对象们前置操作
+            Event::trigger('IMI.SERVERS.CREATE.BEFORE');
+            $mainServer = Config::get('@app.mainServer');
+            if (null === $mainServer)
+            {
+                throw new \RuntimeException('config.mainServer not found');
+            }
+            // 主服务器
+            ServerManager::createServer('main', $mainServer);
+            // 创建监听子服务器端口
+            $subServers = Config::get('@app.subServers', []);
+            if ($subServers)
+            {
+                foreach ($subServers as $name => $config)
+                {
+                    ServerManager::createServer($name, $config, true);
+                }
+            }
+            // 创建服务器对象们后置操作
+            Event::trigger('IMI.SERVERS.CREATE.AFTER');
+
+            /** @var ISwooleServer $server */
+            $server = ServerManager::getServer('main', ISwooleServer::class);
+            $swooleServer = $server->getSwooleServer();
+            // 守护进程支持
+            if ($d)
+            {
+                $options = [
+                    'daemonize' => 1,
+                ];
+                if (true !== $d)
+                {
+                    $options['log_file'] = $d;
+                }
+                $swooleServer->set($options);
+            }
+            $server->start();
+        });
+    }
+
+    /**
+     * 停止服务
+     *
+     * @CommandAction("stop")
+     */
+    public function stop(): void
+    {
+        SwooleImiUtil::stopServer();
+    }
+
+    /**
+     * 重新加载服务
+     *
+     * 重启 Worker 进程，不会导致连接断开，可以让项目文件更改生效
+     *
+     * @CommandAction("reload")
+     * @Option(name="runtime", type=ArgType::BOOL, required=false, default=false, comments="是否更新运行时缓存")
+     */
+    public function reload(bool $runtime): void
+    {
+        if ($runtime)
+        {
+            $imi = new ToolImi($this->command, $this->input, $this->output);
+            $this->output->writeln('<info>Building runtime...</info>');
+            $time = microtime(true);
+            $imi->buildRuntime(null, false);
+            $useTime = microtime(true) - $time;
+            $this->output->writeln("<info>Runtime build complete! {$useTime}s</info>");
+        }
+        SwooleImiUtil::reloadServer();
+    }
+
+    /**
+     * 输出 imi 图标.
+     */
+    public function outImi(): void
+    {
+        $this->output->write('<comment>' . <<<STR
+ _               _
+(_)  _ __ ___   (_)
+| | | '_ ` _ \  | |
+| | | | | | | | | |
+|_| |_| |_| |_| |_|
+
+</comment>
+STR
+        );
+    }
+
+    /**
+     * 输出启动信息.
+     */
+    public function outStartupInfo(): void
+    {
+        $this->output->writeln('<fg=yellow;options=bold>[System]</>');
+        $system = (\defined('PHP_OS_FAMILY') && 'Unknown' !== \PHP_OS_FAMILY) ? \PHP_OS_FAMILY : \PHP_OS;
+        switch ($system)
+        {
+            case 'Linux':
+                $system .= ' - ' . Imi::getLinuxVersion();
+                break;
+            case 'Darwin':
+                $system .= ' - ' . Imi::getDarwinVersion();
+                break;
+            case 'CYGWIN':
+                $system .= ' - ' . Imi::getCygwinVersion();
+                break;
+        }
+        $this->output->writeln('<info>System:</info> ' . $system);
+        if (Imi::isDockerEnvironment())
+        {
+            $this->output->writeln('<info>Virtual machine:</info> Docker');
+        }
+        elseif (Imi::isWSL())
+        {
+            $this->output->writeln('<info>Virtual machine:</info> WSL');
+        }
+        $this->output->writeln('<info>CPU:</info> ' . swoole_cpu_num() . ' Cores');
+        $this->output->writeln('<info>Disk:</info> Free ' . round(@disk_free_space('.') / (1024 * 1024 * 1024), 3) . ' GB / Total ' . round(@disk_total_space('.') / (1024 * 1024 * 1024), 3) . ' GB');
+
+        $this->output->writeln(\PHP_EOL . '<fg=yellow;options=bold>[Network]</>');
+        foreach (swoole_get_local_ip() as $name => $ip)
+        {
+            $this->output->writeln('<info>ip@' . $name . '</info>: ' . $ip);
+        }
+
+        $this->output->writeln(\PHP_EOL . '<fg=yellow;options=bold>[PHP]</>');
+        $this->output->writeln('<info>Version:</info> v' . \PHP_VERSION);
+        $this->output->writeln('<info>Swoole:</info> v' . \SWOOLE_VERSION);
+        $this->output->writeln('<info>imi:</info> ' . App::getImiVersion());
+        $this->output->writeln('<info>Timezone:</info> ' . date_default_timezone_get());
+
+        $this->output->writeln('');
+    }
+}
---
+++ src/SwooleApp.php
@@ -0,0 +1,274 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Swoole;
+
+use Imi\App;
+use Imi\Bean\Annotation;
+use Imi\Bean\BeanManager;
+use Imi\Bean\Scanner;
+use Imi\Cache\CacheManager;
+use Imi\Cli\CliApp;
+use Imi\Cli\ImiArgvInput;
+use Imi\Config;
+use Imi\Core\App\Enum\LoadRuntimeResult;
+use Imi\Event\Event;
+use Imi\Lock\Lock;
+use Imi\Main\Helper;
+use Imi\Pool\PoolManager;
+use Imi\Swoole\Context\CoroutineContextManager;
+use Imi\Swoole\Util\AtomicManager;
+use Imi\Util\Imi;
+use Imi\Util\Process\ProcessAppContexts;
+use Imi\Util\Process\ProcessType;
+use Imi\Worker;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SwooleApp extends CliApp
+{
+    /**
+     * 应用模式的配置.
+     */
+    protected array $appConfig = [
+        'RequestContext' => CoroutineContextManager::class,
+        'Timer'          => SwooleTimer::class,
+        'beans'          => [
+            'ServerUtil' => \Imi\Swoole\Server\Util\LocalServerUtil::class,
+        ],
+    ];
+
+    /**
+     * 构造方法.
+     */
+    public function __construct(string $namespace)
+    {
+        parent::__construct($namespace);
+        $this->cliEventDispatcher->addListener(ConsoleEvents::COMMAND, function (ConsoleCommandEvent $e) {
+            $this->onCommand($e);
+        }, \PHP_INT_MAX - 1000);
+        Event::one('IMI.SCAN_APP', function () {
+            $this->onScanApp();
+        });
+    }
+
+    /**
+     * 获取应用类型.
+     */
+    public function getType(): string
+    {
+        return 'swoole';
+    }
+
+    /**
+     * 运行应用.
+     */
+    public function run(): void
+    {
+        try
+        {
+            $this->cli->run(new ImiArgvInput());
+        }
+        catch (\Swoole\ExitException $e)
+        {
+            throw $e;
+        }
+        catch (\Exception $th)
+        {
+            /** @var \Imi\Log\ErrorLog $errorLog */
+            $errorLog = App::getBean('ErrorLog');
+            $errorLog->onException($th);
+            exit(255);
+        }
+    }
+
+    /**
+     * 加载配置.
+     */
+    public function loadConfig(bool $initDotEnv = true): void
+    {
+        parent::loadConfig(false);
+        $namespace = Config::get('@app.mainServer.namespace');
+        $namespaces = [];
+        if (null !== $namespace)
+        {
+            $namespaces['main'] = $namespace;
+        }
+        foreach (Config::get('@app.subServers', []) as $name => $config)
+        {
+            $namespaces[$name] = $config['namespace'];
+        }
+        foreach ($namespaces as $name => $namespace)
+        {
+            // 加载服务器配置文件
+            foreach (Imi::getNamespacePaths($namespace) as $path)
+            {
+                $fileName = $path . '/config/config.php';
+                if (is_file($fileName))
+                {
+                    Config::addConfig('@server.' . $name, include $fileName);
+                    break;
+                }
+            }
+        }
+        if ($initDotEnv)
+        {
+            $this->loadDotEnv();
+        }
+    }
+
+    /**
+     * 加载运行时.
+     */
+    public function loadRuntime(): int
+    {
+        $this->initRuntime();
+        $input = new ArgvInput();
+        $isServerStart = ('swoole/start' === ($_SERVER['argv'][1] ?? null));
+        if ($isServerStart)
+        {
+            $result = false;
+        }
+        else
+        {
+            // 尝试加载项目运行时
+            $appRuntimeFile = $input->getParameterOption('--app-runtime');
+            if (false !== $appRuntimeFile && Imi::loadRuntimeInfo($appRuntimeFile))
+            {
+                return LoadRuntimeResult::ALL;
+            }
+        }
+        // 尝试加载 imi 框架运行时
+        if ($file = $input->getParameterOption('--imi-runtime'))
+        {
+            // 尝试加载指定 runtime
+            $result = Imi::loadRuntimeInfo($file, true);
+        }
+        else
+        {
+            // 尝试加载默认 runtime
+            $result = Imi::loadRuntimeInfo(Imi::getRuntimePath('imi-runtime'), true);
+        }
+        if ($result)
+        {
+            return LoadRuntimeResult::IMI_LOADED;
+        }
+        else
+        {
+            // 不使用缓存时去扫描
+            Scanner::scanImi();
+            if ($isServerStart)
+            {
+                $imiRuntime = Imi::getRuntimePath('imi-runtime-bak');
+                Imi::buildRuntime($imiRuntime);
+                // 执行命令行生成缓存
+                $cmd = Imi::getImiCmd('imi/buildRuntime', [], [
+                    'imi-runtime' => $imiRuntime,
+                ]);
+                do
+                {
+                    passthru(\Imi\cmd($cmd), $code);
+                    $result = Imi::loadRuntimeInfo(Imi::getRuntimePath('runtime'));
+                    sleep(1);
+                } while (0 !== $code);
+
+                return LoadRuntimeResult::ALL;
+            }
+
+            return LoadRuntimeResult::IMI_LOADED;
+        }
+    }
+
+    /**
+     * 加载入口.
+     */
+    public function loadMain(): void
+    {
+        parent::loadMain();
+        // 服务器们
+        $servers = array_merge(['main' => Config::get('@app.mainServer')], Config::get('@app.subServers', []));
+        foreach ($servers as $serverName => $item)
+        {
+            if ($item)
+            {
+                Helper::getMain($item['namespace'], 'server.' . $serverName);
+            }
+        }
+    }
+
+    /**
+     * 初始化.
+     */
+    public function init(): void
+    {
+        parent::init();
+        register_shutdown_function(function () {
+            App::getBean('Logger')->clear();
+        });
+        foreach (Config::getAliases() as $alias)
+        {
+            // 原子计数初始化
+            AtomicManager::setNames(Config::get($alias . '.atomics', []));
+        }
+        AtomicManager::init();
+        if (BeanManager::get('SwooleWorkerHandler'))
+        {
+            Worker::setWorkerHandler(App::getBean('SwooleWorkerHandler'));
+        }
+        $initCallback = function () {
+            PoolManager::init();
+            CacheManager::init();
+            Lock::init();
+        };
+        Event::one('IMI.PROCESS.BEGIN', $initCallback);
+        Event::one('IMI.MAIN_SERVER.WORKER.START', $initCallback);
+    }
+
+    private function onCommand(ConsoleCommandEvent $e): void
+    {
+        $this->checkEnvironment($e->getOutput());
+        App::set(ProcessAppContexts::PROCESS_NAME, ProcessType::MASTER, true);
+        App::set(ProcessAppContexts::MASTER_PID, getmypid(), true);
+    }
+
+    private function onScanApp(): void
+    {
+        $namespace = Config::get('@app.mainServer.namespace');
+        $namespaces = [];
+        if (null !== $namespace)
+        {
+            $namespaces[] = $namespace;
+        }
+        foreach (Config::get('@app.subServers', []) as $config)
+        {
+            $namespaces[] = $config['namespace'];
+        }
+        Annotation::getInstance()->initByNamespace($namespaces);
+    }
+
+    /**
+     * 检查环境.
+     */
+    private function checkEnvironment(OutputInterface $output): void
+    {
+        // Swoole 检查
+        if (!\extension_loaded('swoole'))
+        {
+            $output->writeln('<error>Swoole extension must be installed!</error>');
+            $output->writeln('<info>Swoole Github:</info> <comment>https://github.com/swoole/swoole-src</comment>');
+            exit(255);
+        }
+        // 短名称检查
+        $useShortname = ini_get_all('swoole')['swoole.use_shortname']['local_value'];
+        $useShortname = strtolower(trim(str_replace('0', '', $useShortname)));
+        if (\in_array($useShortname, ['', 'off', 'false'], true))
+        {
+            $output->writeln('<error>Please enable swoole short name before using imi!</error>');
+            $output->writeln('<info>You can set <comment>swoole.use_shortname = on</comment> into your php.ini.</info>');
+            exit(255);
+        }
+    }
+}
