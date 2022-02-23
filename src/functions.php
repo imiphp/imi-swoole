e3dc19f0b075954f4b38b2bbdfc5450d98a11028
Yurun <admin@yurunsoft.com>
2022-02-23 10:16:17 +0800

    移除 Swoole 短名称检查检查，不启用也可以使用 imi (#281)

--- src/functions.php
+++ src/functions.php
@@ -2,7 +2,32 @@
 
 declare(strict_types=1);
 
+use Imi\Log\Log;
 use Swoole\Coroutine;
+
+if (!\function_exists('\imigo'))
+{
+    /**
+     * 启动一个协程，自动创建和销毁上下文.
+     *
+     * @param mixed $args
+     */
+    function imigo(callable $callable, ...$args): int
+    {
+        $callable = imiCallable($callable);
+
+        return Coroutine::create(static function () use ($callable, $args) {
+            try
+            {
+                $callable(...$args);
+            }
+            catch (\Throwable $th)
+            {
+                Log::error($th);
+            }
+        });
+    }
+}
 
 if (\extension_loaded('swoole'))
 {
