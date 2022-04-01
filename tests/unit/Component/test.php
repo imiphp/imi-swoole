4c66be148d00f53dd37e0cf701bc7cd62bec1429
Yurun <admin@yurunsoft.com>
2022-04-01 09:53:31 +0800

    支持单文件运行 imi (#308)

---
+++ tests/unit/Component/test.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Imi\App;
+use Imi\Db\Db;
+use Imi\Swoole\SwooleApp;
+
+use function Swoole\Coroutine\run;
+
+require \dirname(__DIR__, 3) . '/vendor/autoload.php';
+
+run(static function () {
+    App::runApp(__DIR__, SwooleApp::class, static function () {
+        Db::getInstance(); // 必须，获取连接后，连接池就被启动了
+        echo 'Test swoole quick start', \PHP_EOL;
+    });
+});
