f211cd200290b01eeba22fc65a2aaaa5d89e6a9d
Yurun <admin@yurunsoft.com>
2023-10-27 11:45:59 +0800

    [3.0] 升级 phpunit (#607)

--- tests/unit/HttpServer/Tests/BaseTestCase.php
+++ tests/unit/HttpServer/Tests/BaseTestCase.php
@@ -6,7 +6,7 @@ namespace Imi\Swoole\Test\HttpServer\Tests;
 
 use function Imi\env;
 
-abstract class BaseTest extends \Imi\Swoole\Test\BaseTest
+abstract class BaseTestCase extends \Imi\Swoole\Test\BaseTestCase
 {
     /**
      * 请求主机.
@@ -15,9 +15,9 @@ abstract class BaseTest extends \Imi\Swoole\Test\BaseTest
      */
     protected $host;
 
-    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    public function __construct(string $name)
     {
-        parent::__construct($name, $data, $dataName);
+        parent::__construct($name);
         $this->host = env('HTTP_SERVER_HOST', 'http://127.0.0.1:13000/');
     }
 }
